451201c285055f6930cdd0196946732a295af9d8
skbha <skbha@LAPTOP-DBFNSVJ2>
2023-07-21 11:33:08 +0530

    secondcommit

---
+++ dayone/src/LogicalOperator.java
@@ -0,0 +1,11 @@
+package org.tnsif.operators;
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		System.out.println((5==3) && (14>6));
+		System.out.println((5==3) || (14>6));
+		System.out.println(!5==3) && (14>6));
+
+	}
+
+}
---
+++ dayone/src/org/tnsif/operators/AssignmentOperatorDemo.java
@@ -0,0 +1,12 @@
+package org.tnsif.operators;
+public class AssignmentOperatorDemo {
+
+	public static void main(String[] args) {
+		int x=23,y=45;
+		x+=y;
+		System.out.println(x);
+		
+
+	}
+
+}
